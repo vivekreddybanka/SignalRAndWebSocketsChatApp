bbc8130e7136e58c14ca9e75c3336c6af3f3fbb3
vivekreddybanka <56489987+vivekreddybanka@users.noreply.github.com>
2022-02-02 22:36:53 -0600

    Added  websockets middle ware to project

--- webSocketServer/Program.cs
+++ webSocketServer/Program.cs
@@ -1,5 +1,19 @@
+
+using System.Net.WebSockets;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+// add the websockets
+app.UseWebSockets();
+app.Use(async (context, next) => {
+    if (context.WebSockets.IsWebSocketRequest) {
+        WebSocket websocket =  await context.WebSockets.AcceptWebSocketAsync();
+        Console.WriteLine("Websocket Connected");
+    } 
+    else {
+        await next();
+    }
+});
 
 app.MapGet("/", () => "Hello World!");
 
