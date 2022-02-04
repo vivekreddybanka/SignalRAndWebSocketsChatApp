388552240d9285e8869b59de5ccf5f8351b4e769
vivekreddybanka <bankavivek94@gmail.com>
2022-02-03 22:42:11 -0600

    Added sending message and socket reading.

--- webSocketServer/Program.cs
+++ webSocketServer/Program.cs
@@ -1,5 +1,6 @@
    
 using System.Net.WebSockets;
+using System.Threading;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -9,6 +10,16 @@ app.Use(async (context, next) => {
     if (context.WebSockets.IsWebSocketRequest) {
         WebSocket websocket =  await context.WebSockets.AcceptWebSocketAsync();
         Console.WriteLine("Websocket Connected");
+        await ReceiveMessage(websocket,async (result, buffer) => {
+
+            if (result.MessageType == WebSocketMessageType.Text) {
+                Console.WriteLine("Message received ");
+            }
+            else if (result.MessageType == WebSocketMessageType.Close) {
+                Console.WriteLine("received Close Message");
+                return;
+            }
+        });
     } 
     else {
         Console.WriteLine("Hello from the 2nd request");
@@ -29,4 +40,13 @@ void WriteRequestParam(HttpContext context) {
             Console.WriteLine("-->" + hdr.Key + " : " +hdr.Value);
         }
     }
+}
+
+async Task ReceiveMessage(WebSocket socket, Action<WebSocketReceiveResult, byte [] > handleMessage) {
+     var buffer = new byte[1024 * 4];
+     while(socket.State == WebSocketState.Open) {
+         var result = await socket.ReceiveAsync(buffer: new ArraySegment<byte>(buffer),
+         cancellationToken: CancellationToken.None);
+         handleMessage(result,buffer);
+     }
 }
