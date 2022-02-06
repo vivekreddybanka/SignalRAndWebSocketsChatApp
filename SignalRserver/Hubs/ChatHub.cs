c72d5d014a4b5dc386f59ea008b41a087f182461
vivekreddybanka <bankavivek94@gmail.com>
2022-02-06 10:07:04 -0600

    Added the ChatHub Class to handle Messages.

--- SignalRserver/Hubs/ChatHub.cs
+++ SignalRserver/Hubs/ChatHub.cs
@@ -8,8 +8,25 @@ namespace SignalRserver
     public class ChatHub: Hub {
         public override Task OnConnectedAsync() 
         {
-            Console.WriteLine("Connected Established " +  Context.ConnectionID);
+            Console.WriteLine("Connected Established " +  Context.ConnectionId);
+            Clients.Client(Context.ConnectionId).SendAsync("ReceiveClientID",Context.ConnectionId);
+            return base.OnConnectedAsync();
         }
+
+        public async Task SendMessageAsync(string Message) {
+            var routeob = JsonConvert.DeserializeObject<dynamic>(Message);
+            string toClient = routeob.To; 
+            Console.WriteLine("Message Recived On: " + Context.ConnectionId);
+
+            if(!string.IsNullOrEmpty(toClient)) {
+
+                await Clients.Client(toClient).SendAsync("receiveMessage" + Message);
+            }
+            else {
+                await Clients.All.SendAsync("ReceiveMessage" +  Message);
+            }
+        }
+
     }
 
 }
