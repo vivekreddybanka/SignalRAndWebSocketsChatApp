0664bbff16bdaebfbec28bcd8bc4837deed202ca
vivek.banka <vivek.banka@ies-co.com>
2022-02-08 09:02:28 -0600

    Added the signal R Client application  client.

--- SignalRserver/Hubs/ChatHub.cs
+++ SignalRserver/Hubs/ChatHub.cs
@@ -5,25 +5,29 @@ using Newtonsoft.Json;
 
 namespace SignalRserver
 {
-    public class ChatHub: Hub {
-        public override Task OnConnectedAsync() 
+    public class ChatHub : Hub
+    {
+        public override Task OnConnectedAsync()
         {
-            Console.WriteLine("Connected Established " +  Context.ConnectionId);
-            Clients.Client(Context.ConnectionId).SendAsync("ReceiveClientID",Context.ConnectionId);
+            Console.WriteLine("Connected Established " + Context.ConnectionId);
+            Clients.Client(Context.ConnectionId).SendAsync("ReceiveClientID", Context.ConnectionId);
             return base.OnConnectedAsync();
         }
 
-        public async Task SendMessageAsync(string Message) {
+        public async Task SendMessageAsync(string Message)
+        {
             var routeob = JsonConvert.DeserializeObject<dynamic>(Message);
-            string toClient = routeob.To; 
+            string toClient = routeob.To;
             Console.WriteLine("Message Recived On: " + Context.ConnectionId);
 
-            if(!string.IsNullOrEmpty(toClient)) {
+            if (!string.IsNullOrEmpty(toClient))
+            {
 
                 await Clients.Client(toClient).SendAsync("receiveMessage" + Message);
             }
-            else {
-                await Clients.All.SendAsync("ReceiveMessage" +  Message);
+            else
+            {
+                await Clients.All.SendAsync("ReceiveMessage" + Message);
             }
         }
 
